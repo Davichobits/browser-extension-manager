c4ae530f7cf16b4ac11aaa27de6d838253eeadd2
Davichobits <davidruizdeveloper@gmail.com>
2025-07-10 15:17:14 -0500

    fix: button export

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { Header } from "./header";
 export { Filter } from "./filter";
-export { Button } from "./Button";
 export { Card } from "./card";
 export { CardContainer } from "./card-container";
+import { Button } from "./button";
